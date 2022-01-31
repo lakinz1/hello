8dc12274a4b2d3367fd93e08cf184e8a53513daf
Your Name <ted26@huskers.unl.edu>
2022-01-31 23:34:27 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ted Vo
+ *         
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ted Vo!\n");
+
+  return 0;
+}
