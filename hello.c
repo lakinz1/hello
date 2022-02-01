5034e4f336a68673b7e4f4e0bc5320248df17b95
Marvin2O03 <morellana5@huskers.unl.edu>
2022-02-01 01:07:49 +0000

    added two lines

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 /**
  * Author: Ted Vo
- *         
+ *
  * Date: 2022/01/28
  *
  * A simple hello world program in C
@@ -13,6 +13,9 @@ int main(int argc, char **argv) {
 
   printf("My name is Ted Vo!\n");
   printf("My major is Computer Engineering.\n");
+
+  printf("My name is Marvin Orellana\n");
+  printf("My major is Computer Engineering\n");
 
   return 0;
 }
